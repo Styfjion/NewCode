6866b55d5f6099f86394dea898b308d7fe49bc7b
Styfjion <17717092026@163.com>
2022-04-21 01:32:13 +0800

    补充

--- CPP/Lee1114.cpp
+++ CPP/Lee1114.cpp
@@ -2,6 +2,7 @@
 // Created by yangf on 2022/3/18.
 //
 
+#include <iostream>
 #include <atomic>
 #include <thread>
 #include <functional>
@@ -12,21 +13,45 @@ class Foo {
     atomic<bool> a{ false };
     atomic<bool> b{ false };
 public:
-    void first(function<void()> printFirst) {
+    void first(const function<void()>& printFirst) {
         printFirst();
         a = true;
     }
 
-    void second(function<void()> printSecond) {
+    void second(const function<void()>& printSecond) {
         while (!a)
             this_thread::sleep_for(chrono::milliseconds(1));
         printSecond();
         b = true;
     }
 
-    void third(function<void()> printThird) {
+    void third(const function<void()>& printThird) {
         while (!b)
             this_thread::sleep_for(chrono::milliseconds(1));
         printThird();
     }
 };
+
+void printFirst() {
+    cout << "printFirst" << endl;
+}
+
+void printSecond() {
+    cout << "printSecond" << endl;
+}
+
+void printThird() {
+    cout << "printThird" << endl;
+}
+
+int main()
+{
+    Foo foo;
+    thread t1(&Foo::second, &foo, std::ref(printSecond));
+    thread t2(&Foo::first, &foo, std::ref(printFirst));
+    thread t3(&Foo::third, &foo, std::ref(printThird));
+    t1.join();
+    t2.join();
+    t3.join();
+    return 0;
+}
--- CPP/Lee1114Two.cpp
+++ CPP/Lee1114Two.cpp
@@ -1,6 +1,8 @@
 //
 // Created by yangf on 2022/3/18.
 //
+#include <iostream>
+#include <thread>
 #include <functional>
 #include <condition_variable>
 
@@ -11,13 +13,13 @@ class Foo {
     mutex mtx;
     int k = 0;
 public:
-    void first(function<void()> printFirst) {
+    void first(const function<void()>& printFirst) {
         printFirst();
         k = 1;
         cv.notify_all();    // 通知其他所有在等待唤醒队列中的线程
     }
 
-    void second(function<void()> printSecond) {
+    void second(const function<void()>& printSecond) {
         unique_lock<mutex> lock(mtx);   // lock mtx
         cv.wait(lock, [this](){ return k == 1; });  // unlock mtx，并阻塞等待唤醒通知，需要满足 k == 1 才能继续运行
         printSecond();
@@ -25,9 +27,33 @@ public:
         cv.notify_one();    // 随机通知一个（unspecified）在等待唤醒队列中的线程
     }
 
-    void third(function<void()> printThird) {
+    void third(const function<void()>& printThird) {
         unique_lock<mutex> lock(mtx);   // lock mtx
         cv.wait(lock, [this](){ return k == 2; });  // unlock mtx，并阻塞等待唤醒通知，需要满足 k == 2 才能继续运行
         printThird();
     }
 };
+
+void printFirst() {
+    cout << "printFirst" << endl;
+}
+
+void printSecond() {
+    cout << "printSecond" << endl;
+}
+
+void printThird() {
+    cout << "printThird" << endl;
+}
+
+int main()
+{
+    Foo foo;
+    thread t1(&Foo::second, &foo, std::ref(printSecond));
+    thread t2(&Foo::first, &foo, std::ref(printFirst));
+    thread t3(&Foo::third, &foo, std::ref(printThird));
+    t1.join();
+    t2.join();
+    t3.join();
+    return 0;
+}
