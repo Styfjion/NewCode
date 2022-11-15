d856a830ea0958c39e2860e29b6d111bbf603629
Styfjion <33060090+Styfjion@users.noreply.github.com>
2022-11-15 19:30:48 +0800

    Update Widget.h

--- CPP/NewHandlerTest/Widget.h
+++ CPP/NewHandlerTest/Widget.h
@@ -1,14 +1,11 @@
 #ifndef TEST_WIDGET_H
 #define TEST_WIDGET_H
-#include <new>
 
-class Widget {
+#include "NewHandlerSupport.h"
+
+class Widget : public NewHandlerSupport<Widget> {
 public:
-    static std::new_handler set_new_handler(std::new_handler p) noexcept;
-    static void* operator new(std::size_t size) noexcept;
-    static void *operator new[](std::size_t size) noexcept;
-private:
-    static std::new_handler currentHandler;
+    Widget() { std::cout << "Widget init" << std::endl; }
 };
 
 
