9f12972ca29dbb120680b532d0a4af8418b9dec1
Styfjion <33060090+Styfjion@users.noreply.github.com>
2023-05-25 17:15:18 +0800

    Create readFile.js

---
+++ Example/ReadFileByLine/readFile.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('test.env'),
+    output: process.stdout,
+    terminal: false
+});
+
+console.log("hello, world")
+let ret = []
+rl.on('line', (line) => {
+    let splitLines = line.split('=')
+    ret.push(splitLines[0])
+})
+
+rl.on('close', () => {
+    console.log(ret.join(','))
+})
