c2efd825b4a149c1d436128f8955731e84a8f601
Styfjion <33060090+Styfjion@users.noreply.github.com>
2023-06-06 15:09:25 +0800

    Create asynctest.ts

---
+++ Example/Async/asynctest.ts
@@ -0,0 +1,32 @@
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
+async function g(): Promise<any> {
+    console.log("g 1")
+    await sleep(1000)
+    console.log("g 2")
+}
+
+async function f(): Promise<any> {
+    let a = g()
+    console.log("f 1")
+    await a
+    await sleep(1000)
+    console.log("f 2")
+}
+
+async function out(): Promise<any> {
+    let b = f()
+    console.log("start")
+    await b
+    console.log("finish")
+}
+
+out().then().catch()
+
+// g 1
+// f 1
+// start
+// g 2
+// f 2
+// finish
+
