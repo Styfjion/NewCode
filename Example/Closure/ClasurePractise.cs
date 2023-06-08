bb357e6ab1fcbb007e50070a0197dcd95414207b
Styfjion <33060090+Styfjion@users.noreply.github.com>
2023-06-08 17:20:04 +0800

    Create ClasurePractise.cs

---
+++ Example/Closure/ClasurePractise.cs
@@ -0,0 +1,27 @@
+public class ClasurePractise
+{
+    public Func<int>[] CreateCounters(int init)
+    {
+        int curValue = init;
+        var add = () => ++curValue;
+        var dec = () => --curValue;
+        var reset = () =>
+        {
+            curValue = init;
+            return curValue;
+        };
+        return new []{add, dec, reset};
+    }
+
+    public static void Main()
+    {
+        var sol = new ClasurePractise();
+        var counters = sol.CreateCounters(5);
+        Console.WriteLine(counters[0]());
+        Console.WriteLine(counters[2]());
+        Console.WriteLine(counters[1]());
+    }
+}
+// 6
+// 5
+// 4
