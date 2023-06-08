4dce295113b982efae76434fb36b433c5fbc45f0
Styfjion <33060090+Styfjion@users.noreply.github.com>
2023-06-08 17:20:47 +0800

    Create Closure.cpp

---
+++ Example/Closure/Closure.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+vector<function<int()>> createCounter(int init) {
+    static int curVal = init;
+    static int prime = init;
+    auto add = [&](){return ++curVal;};
+    auto dec = [&]() {return --curVal;};
+    auto reset = [&](){
+        curVal = prime;
+        return curVal;
+    };
+    return {add, dec, reset};
+}
+
+int main() {
+    auto sol = createCounter(5);
+    cout << sol[0]() << endl;
+    cout << sol[2]() << endl;
+    cout << sol[1]() << endl;
+}
+// 6
+// 5
+// 4
